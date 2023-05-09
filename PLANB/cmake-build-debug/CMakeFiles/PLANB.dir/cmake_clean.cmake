e82e059a57d725c9dd2d05a1eedbca94530f585b
Dídac <didaccis@gmail.com>
2023-05-09 21:39:09 +0200

    Changed user list from dynamic array to linked list, still some errors when listing (printing) users on console

---
+++ PLANB/cmake-build-debug/CMakeFiles/PLANB.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PLANB.dir/main.c.obj"
+  "CMakeFiles/PLANB.dir/main.c.obj.d"
+  "CMakeFiles/PLANB.dir/sources/users.c.obj"
+  "CMakeFiles/PLANB.dir/sources/users.c.obj.d"
+  "PLANB.exe"
+  "PLANB.exe.manifest"
+  "PLANB.pdb"
+  "libPLANB.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PLANB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
