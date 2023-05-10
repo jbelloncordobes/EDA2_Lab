a13a46c1a2bb345e9bdcff44eb3a969fc19ca378
jbelloncordobes <jbelloncordobes@gmail.com>
2023-05-10 20:29:37 +0200

    Intentando

---
+++ cmake-build-debug/CMakeFiles/EDA2_Lab.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Hp/Documents/EDA2/Terminal/EDA2_Lab/main.c" "CMakeFiles/EDA2_Lab.dir/main.c.obj" "gcc" "CMakeFiles/EDA2_Lab.dir/main.c.obj.d"
+  "C:/Users/Hp/Documents/EDA2/Terminal/EDA2_Lab/sources/interface.c" "CMakeFiles/EDA2_Lab.dir/sources/interface.c.obj" "gcc" "CMakeFiles/EDA2_Lab.dir/sources/interface.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/EDA2_Lab.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for EDA2_Lab.
+# This may be replaced when dependencies are built.
